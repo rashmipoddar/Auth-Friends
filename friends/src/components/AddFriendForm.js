2b345933a19017454d306ce48d4c44792198649c
Rashmi <poddar.rashmi@gmail.com>
2019-10-15 19:07:58 -0400

    Created custom hook for forms

---
+++ friends/src/components/AddFriendForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AddFriendForm = () => {
+  return (  
+    <form>
+      <input 
+        name='name'
+        type='text'
+        placeholder='Name'
+        value={}
+        onChange={onChange}
+      />
+    </form>
+  )
+} 
+
+export default AddFriendForm;
